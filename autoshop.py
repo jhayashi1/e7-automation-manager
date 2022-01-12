8b7d1c718d3505d324d3ccbbde71b0463ffbe0ea
jhayashi1 <jhayashi@iastate.edu>
2022-01-11 21:48:05 -0600

    now refreshes shop and scrolls

--- autoshop.py
+++ autoshop.py
@@ -4,69 +4,97 @@ import utils
 import keyboard
 
 SCREENCAP_PATH = './screenshots/screen.png'
+REFRESH_CONFIRM_PATH = './screenshots/refresh_confirm.png'
 
-def click_loop(device, x, y, width, height, confirm_path, is_buy_confirm):
-    #Get random offsets based if the confirm menu is currently displayed
-    if is_buy_confirm:
-        x_offset, y_offset = utils.get_rand_buy_offsets(width, height)
-        confidence = 0.30
-    else:
-        x_offset, y_offset = utils.get_rand_offsets(width, height)
-        confidence = 0.40
+SHOP_ITEM_CONFIDENCE = 0.15
+CONFIRM_CONFIDENCE = 0.30
+REFRESH_CONFIRM_CONFIDENCE = 0.60
+
+#TODO Remove x and y from parameters and pass coordinates instead
+# device: device being operated on
+# x, y: x and y coordinates of the button to be clicked
+# confirm_button_path: path to the image containing the button to be checked for
+# offset_id: id for getting random offsets based on button being clicked
+# confidence: required confidence for image to pass
+def click_loop(device, x, y, confirm_button_path, offset_id, confidence):
+    x_offset, y_offset = utils.get_rand_offsets(device, offset_id)
 
     #Get random click length
     click_len = utils.get_rand_click_length()
 
     #Click on buy button
-    device.shell(utils.get_adb_click_str(x + x_offset, y + y_offset, click_len))
+    device.shell(utils.get_adb_swipe_str(x + x_offset, y + y_offset, x + x_offset, y + y_offset, click_len))
 
     #Wait for screen to appear and take new screenshot
     time.sleep(random.uniform(0.25, 0.75))
     utils.get_screencap(device, 'screen.png')
 
     #Look for confirm button
-    return utils.compare_images(confirm_path, SCREENCAP_PATH, confidence)
+    return utils.compare_images(confirm_button_path, SCREENCAP_PATH, confidence)
 
 def refresh_shop(device):
-    pass
+    confirm_coords = None
+
+    while not confirm_coords:
+        confirm_coords = click_loop(device, 0, 0, REFRESH_CONFIRM_PATH, 2, REFRESH_CONFIRM_CONFIDENCE)
+
+    x, y = confirm_coords    
+
+    #Loop while the confirm button is still on the screen
+    while confirm_coords:
+        confirm_coords = click_loop(device, x, y, REFRESH_CONFIRM_PATH, 3, CONFIRM_CONFIDENCE)
 
 def parse_shop(device):
-        resolution = device.shell('wm size').split()[-1].split('x')
-        width, height = resolution[:2]
-        utils.get_screencap(device, 'screen.png')
         has_scrolled = False
 
         while keyboard.is_pressed('q') == False:
+            if has_scrolled:
+                scroll_len = utils.get_rand_scroll_length()
+                scroll_coords = utils.get_rand_scroll(device, True)
+
+                print('scrolling from (' + str(int(scroll_coords[0])) + ', ' + str(int(scroll_coords[2])) + ') to (' + str(int(scroll_coords[0])) + ', ' + str(int(scroll_coords[3])) + ')')
+
+                device.shell(utils.get_adb_swipe_str(scroll_coords[0], scroll_coords[1], scroll_coords[2], scroll_coords[3], scroll_len))
+                time.sleep(random.uniform(0.25, 0.5))
+
+            utils.get_screencap(device, 'screen.png')
+
             #Look for covenants first, then mystics
+            time.sleep(random.uniform(0.4, 0.75))
+
             for i in range(2):
                 if i == 0:
                     image_path = './screenshots/covenant.png'
-                    confirm_path = './screenshots/covenant_confirm.png'
+                    confirm_button_path = './screenshots/covenant_confirm.png'
                 else:
                     image_path = './screenshots/mystic.png'
-                    confirm_path = './screenshots/mystic_confirm.png'
-
+                    confirm_button_path = './screenshots/mystic_confirm.png'
+ 
 
                 #Get coordinates for valid items in shop
-                coords = utils.compare_images(image_path, SCREENCAP_PATH, 0.40)
+                coords = utils.compare_images(image_path, SCREENCAP_PATH, SHOP_ITEM_CONFIDENCE)
 
                 #Check if there is a valid item
                 if not coords:
                     continue
-
-                #Get x and y values
+                
                 x, y = coords
                 confirm_coords = None
-                
+
                 #Loop until the confirm button is found
                 while not confirm_coords:
-                    confirm_coords = click_loop(device, x, y, width, height, confirm_path, False)
-                
-                #Get x and y values
-                buy_x, buy_y = confirm_coords
+                    confirm_coords = click_loop(device, x, y, confirm_button_path, 0, CONFIRM_CONFIDENCE)
 
+                x, y = confirm_coords
+
+                #Loop until confirm button is clicked
                 while confirm_coords:
-                    confirm_coords = click_loop(device, buy_x, buy_y, width, height, confirm_path, True)
+                    confirm_coords = click_loop(device, x, y, confirm_button_path, 1, CONFIRM_CONFIDENCE)
                 
-                if has_scrolled:
-                    refresh_shop(device)
+                time.sleep(random.uniform(1.0, 1.5))
+            
+            if has_scrolled:
+                refresh_shop(device)
+                has_scrolled = False
+            else:
+                has_scrolled = True
--- utils.py
+++ utils.py
@@ -4,6 +4,14 @@ import cv2
 MIN_CLICK_LEN = 100
 MAX_CLICK_LEN = 200
 
+MIN_SCROLL_LEN = 50
+MAX_SCROLL_LEN = 52
+
+SCROLL_MIN_X_OFFSET = 0.52
+SCROLL_MAX_X_OFFSET = 0.81
+SCROLL_MIN_Y_OFFSET = 0.13
+SCROLL_MAX_Y_OFFSET = 0.96
+
 WIDTH_MIN_OFFSET = 0.41
 WIDTH_MAX_OFFSET = 0.53
 HEIGHT_MIN_OFFSET = 0.07
@@ -14,6 +22,16 @@ BUY_MAX_X_OFFSET = 0.2
 BUY_MIN_Y_OFFSET = 0.01
 BUY_MAX_Y_OFFSET = 0.07
 
+REFRESH_MIN_X_OFFSET = 0.07
+REFRESH_MAX_X_OFFSET = 0.27
+REFRESH_MIN_Y_OFFSET = 0.89
+REFRESH_MAX_Y_OFFSET = 0.95
+
+REFRESH_CONFIRM_MIN_X_OFFSET = 0.03
+REFRESH_CONFIRM_MAX_X_OFFSET = 0.15
+REFRESH_CONFIRM_MIN_Y_OFFSET = 0.01
+REFRESH_CONFIRM_MAX_Y_OFFSET = 0.07
+
 def get_device(devices):
     for device in devices:
         try:
@@ -21,6 +39,9 @@ def get_device(devices):
             return device
         except:
             print('Device offline, trying next device...')
+
+def get_resolution(device):
+    return device.shell('wm size').split()[-1].split('x')
 
 def get_screencap(device, name):
     image = device.screencap()
@@ -50,16 +71,44 @@ def compare_images(image_path, sample_path, confidence):
 
     return coords
 
-def get_rand_offsets(width, height):
-    result = [float(width) * random.uniform(WIDTH_MIN_OFFSET, WIDTH_MAX_OFFSET), float(height) * random.uniform(HEIGHT_MIN_OFFSET, HEIGHT_MAX_OFFSET)]
+def get_rand_offsets(device, offset_id):
+    width, height = get_resolution(device)
+    #0 = buy button, 1 = buy confirm button, 2 = refresh button, 3 = refresh confirm button
+    offset_arr = offset_id_cases(offset_id)
+
+    result = [float(width) * random.uniform(offset_arr[0], offset_arr[1]), float(height) * random.uniform(offset_arr[2], offset_arr[3])]
     return result
 
-def get_rand_buy_offsets(width, height):
-    result = [float(width) * random.uniform(BUY_MIN_X_OFFSET, BUY_MAX_X_OFFSET), float(height) * random.uniform(BUY_MIN_Y_OFFSET, BUY_MAX_Y_OFFSET)]
-    return result
+def offset_id_cases(id):
+    return {
+        0 : [WIDTH_MIN_OFFSET, WIDTH_MAX_OFFSET, HEIGHT_MIN_OFFSET, HEIGHT_MAX_OFFSET],
+        1 : [BUY_MIN_X_OFFSET, BUY_MAX_X_OFFSET, BUY_MIN_Y_OFFSET, BUY_MAX_Y_OFFSET],
+        2 : [REFRESH_MIN_X_OFFSET, REFRESH_MAX_X_OFFSET, REFRESH_MIN_Y_OFFSET, REFRESH_MAX_Y_OFFSET],
+        3 : [REFRESH_CONFIRM_MIN_X_OFFSET, REFRESH_CONFIRM_MAX_X_OFFSET, REFRESH_CONFIRM_MIN_Y_OFFSET, REFRESH_CONFIRM_MAX_Y_OFFSET]
+    }[id]
 
-def get_adb_click_str(x, y, click_len):
-    return 'input touchscreen swipe ' + str(x) + ' ' + str(y) + ' ' + str(x) + ' ' + str(y) + ' ' + str(click_len)
+def get_adb_swipe_str(x1, y1, x2, y2, click_len):
+    return 'input touchscreen swipe ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + str(click_len)
 
 def get_rand_click_length():
     return random.randrange(MIN_CLICK_LEN, MAX_CLICK_LEN)
+
+def get_rand_scroll_length():
+    return random.randrange(MIN_SCROLL_LEN, MAX_SCROLL_LEN)
+
+def get_rand_scroll(device, is_down):
+    width, height = get_resolution(device)
+    x_offset = random.uniform(SCROLL_MIN_X_OFFSET, SCROLL_MAX_X_OFFSET)
+    rand_x_offset_offset = x_offset / 10
+    avg_y = (SCROLL_MIN_Y_OFFSET + SCROLL_MAX_Y_OFFSET) / 2
+
+    result = [float(width) * x_offset, float(height) * random.uniform(SCROLL_MIN_Y_OFFSET, avg_y - (avg_y / 10)),
+    float(width) * (x_offset - (rand_x_offset_offset) + (random.uniform(0.0, rand_x_offset_offset))),
+    float(height) * random.uniform(avg_y + (avg_y / 10), SCROLL_MAX_Y_OFFSET)]
+
+    #Change y positions if scrolling down
+    if is_down:
+        result[1], result[3] = result[3], result[1]
+
+    return result
+
